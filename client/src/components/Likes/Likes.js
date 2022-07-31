8619d2ebb26cb3b06e6b5ece58cf3baf58c2a591
Desislava Deneva <99611357+desislava-deneva@users.noreply.github.com>
2022-07-31 22:30:24 +0300

    add like authentication

--- client/src/components/Likes/Likes.js
+++ client/src/components/Likes/Likes.js
@@ -3,41 +3,44 @@ import cn from "classnames";
 import { ReactComponent as Hand } from "./hand.svg";
 import { useParams } from "react-router-dom";
 import * as api from '../../api/data';
-import { getUserId } from "../../api/authServices";
 
 import './Likes.css'
-
-
-
-
 
 export const LikeButton = () => {
   const [liked, setLiked] = useState(null);
   const [clicked, setClicked] = useState(false);
   const [likes, setLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
   const params = useParams();
   const id = params.id;
+  const userId = sessionStorage.userId;
 
   useEffect(()=>{
     api.getLikesById(id)
     .then(data=>{
       console.log(data)
       setLikes(data.likes.length);
+      if(data.likes.includes(userId)){
+        setIsLiked(true)
+      }
     })
   })
 
+
   const likeHandler = (isCliked) => {
-    if (isCliked) {
+    if (isCliked && isLiked ) {
     
       api.postUnLikesById(id)
       .then((data) => {
         setLikes(data.likes.length)
       })
       
-    } else {
+    } else{
       api.postLikeById(id)
       .then((data) => {
         setLikes(data.likes.length)
+
       })
       
       .catch((err) => {
@@ -47,7 +50,7 @@ export const LikeButton = () => {
 
     setClicked(!clicked);
   }
-
+  
   return (
     <button
       onClick={() => {
