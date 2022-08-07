bfb73c752a2b83e956eaac6fa16118cd35d4326b
Desislava Deneva <99611357+desislava-deneva@users.noreply.github.com>
2022-08-08 01:24:43 +0300

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,30 +1,25 @@
 import './Home.css';
 import { Pet } from '../Pet/Pet'
-import { useState, useEffect } from 'react';
-import * as api from '../../api/data';
+import { useState, useEffect, useContext } from 'react';
+import { PetContext } from '../../contexts/PetContext';
 
-export const Home = (props) => {
+export const Home = () => {
 
-    const [pets, setPets] = useState([]);
+    const [lastAddPets, setLastAddPets] = useState([]);
+    const { pets } = useContext(PetContext);
 
-    useEffect( () => {
-     api.getPets()
-      .then(result=>{
-        result.reverse()
-        result.length = 4;
-        setPets(result)
-      });
-
-    },[])
+    useEffect(() => {
+        lastAddPets.reverse()
+        lastAddPets.length = 4;
+        setLastAddPets(pets)
+    }, [lastAddPets, pets])
 
     return (
-
         <div className="home-list">
             <div className="home-pets">
                 <h1>Last lost pets</h1>
-                {pets? pets.map(pet => <Pet key={pet._id} pet={pet} /> ): <h1>No pets in database</h1>}
+                {pets ? pets.map(pet => <Pet key={pet._id} pet={pet} />) : <h1>No pets in database</h1>}
             </div>
         </div>
-
     )
 }
